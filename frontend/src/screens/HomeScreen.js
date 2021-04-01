6d8292df161c1ad8d03343dcae5a5adefd0c4178
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-04-01 19:25:54 -0300

    adding redux context to HomeScreen Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,19 @@
-import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
-import axios from 'axios'
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productAction';
+import { useEffect } from 'react';
 
 
 const HomeScreen = () => {
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true)
-                const { data } = await axios.get('/api/products')
-                setLoading(false)
-                setProducts(data)
-            }
-            catch (err) {
-                setError(err.message)
-                setLoading(false)
-            }
-        }
-        fetchData()
+        dispatch(listProducts())
     }, [])
 
     return (
